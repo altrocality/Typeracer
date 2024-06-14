d6eab4bebca5d6924b35e59a1d9fd475560d16b3
altrocality <142731164+altrocality@users.noreply.github.com>
2024-06-14 20:31:35 +1000

    Fixed start times

--- more_display_modes.user.js
+++ more_display_modes.user.js
@@ -1,28 +1,31 @@
 // ==UserScript==
-// @name         Typeracer: Peek Mode
+// @name         Typeracer: More Display Modes
 // @namespace    http://tampermonkey.net/
-// @version      1.1
+// @version      1.1.2
+// @downloadURL  https://raw.githubusercontent.com/altrocality/Typeracer/master/more_display_modes.user.js
+// @updateURL    https://raw.githubusercontent.com/altrocality/Typeracer/master/more_display_modes.user.js
 // @description  Shows the current and next 'n' words
 // @author       altrocality
 // @match        https://play.typeracer.com/*
 // @match        https://staging.typeracer.com/*
 // @icon         https://www.google.com/s2/favicons?domain=typeracer.com
 // ==/UserScript==
-/*
-/////////////////////////////////////////////////////////////////////////////
-To change the settings, press 'change display format' when in a race.
 
-If you're using poem's smooth caret, access the menu by:
-    1. Selecting the input box
-    2. Press shift + tab, then enter
 /////////////////////////////////////////////////////////////////////////////
-*/
+//  To change the settings, press 'change display format' when in a race.  //
+//                                                                         //
+//  If you're using poem's smooth caret, to access the menu:               //
+//      1. Select the quote input box                                      //
+//      2. Press shift + tab, then enter                                   //
+/////////////////////////////////////////////////////////////////////////////
+
 var peek;
 var enabled;
 
 var convertToBool = "true";
 var racing = false;
 var initHeight;
+var switchedToMain = false;
 const doPeekNext = new MutationObserver(peekNext);
 const newTheme = typeof com_typeracer_redesign_Redesign === "function";
 
@@ -71,7 +74,96 @@ function raceEnd() {
     racing = false;
 }
 
-function status() {
+function addNext(textDiv, textSpans) {
+    var hiddenSpan = textSpans[textSpans.length-1];
+    var peekedWords = hiddenSpan.textContent.split(" ", peek+1).join(" ")+" ";
+    var shownWords = document.createTextNode(peekedWords);
+    var currPos = textSpans[textSpans.length-2];
+    var typo = document.getElementsByClassName('txtInput txtInput-error')[0];
+
+    if (currPos.textContent != peekedWords) {
+        var shownSpan = document.createElement("span");
+        if (currPos.textContent == " ") {
+            // At end of word
+            shownWords = document.createTextNode(hiddenSpan.textContent.split(" ", peek).join(" ")+" ");
+            shownSpan.appendChild(shownWords);
+            currPos.parentNode.insertBefore(shownSpan, currPos.nextSibling);
+            shownSpan.className = "nextWords";
+        }
+        if ((currPos.textContent.match(/ /g)||[]).length != peek) {
+            shownSpan.appendChild(shownWords);
+            currPos.parentNode.insertBefore(shownSpan, currPos.nextSibling);
+            shownSpan.className = "nextWords";
+        }
+    }
+}
+
+function peekNext() {
+    var textDiv = document.querySelector('.inputPanel tbody tr td table tbody tr td div');
+    var textSpans = document.querySelectorAll('.inputPanel tbody tr td table tbody tr td div div span');
+
+    // Maintaining initial height
+    textDiv.setAttribute("style", "height: "+initHeight+"px");
+    textDiv.style.position = "relative";
+
+    var hiddenSpan = textSpans[textSpans.length-1];
+    var hiddenSpanNum = (hiddenSpan.textContent.match(/ /g)||[]).length;
+    if (peek >= hiddenSpanNum) {
+        // Read ahead covers rest of text -> quit
+        return;
+    }
+    // Disabled during race?
+    if (!enabled) {
+        document.getElementsByClassName("nextWords")[0].remove();
+        raceEnd();
+        return;
+    }
+    // Hide upcoming words
+    textDiv.style.visibility = "hidden";
+    for (var i = 0; i < (textSpans.length-1); i++) {
+        textSpans[i].style.visibility = "visible";
+    }
+
+    // Don't hide smooth caret in new theme
+    var smoothCaret = document.getElementById("smoothCaret");
+    if (newTheme && smoothCaret) {
+        document.getElementById("smoothCaret").style.visibility = "visible";
+    }
+    addNext(textDiv, textSpans);
+}
+
+loadSettings();
+
+// Detecting game status
+var observer = new MutationObserver(function() {
+    // Modified from github.com/PoemOnTyperacer/tampermonkey/blob/master/pacemaker.user.js lines 321-339 tyyyy :)
+    let gameStatusLabels = document.getElementsByClassName('gameStatusLabel');
+    let gameStatus = ((gameStatusLabels || [])[0] || {}).innerHTML || '';
+    if (!racing && (gameStatusLabels.length > 0 && (gameStatus == 'Go!' || gameStatus.startsWith('The race is on') || gameStatus == 'The race is about to start!'))) {
+        let practiceTitleEl = document.getElementsByClassName('roomSection')[0];
+        if(practiceTitleEl && practiceTitleEl.innerText.startsWith('Practice')) {
+            if (!switchedToMain) {
+                raceStart();
+            }
+            document.getElementsByClassName('gwt-Anchor')[4].addEventListener('click', function () {
+                raceEnd();
+                switchedToMain = true;
+            });
+        } else {
+            // In a public lobby
+            switchedToMain = false;
+            var countdown = document.getElementsByClassName('lightLabel')[0].parentNode.nextSibling.textContent;
+            if (countdown == ":03") {
+                raceStart();
+                peekNext();
+            }
+        }
+    }
+
+    if(racing && ((gameStatusLabels.length==0) || (document.getElementsByClassName('rank')[0].innerText=='Done!'|| document.getElementsByClassName('rank')[0].innerText.includes('Place')))){
+        raceEnd();
+    }
+
     // Config
     var displayModes = document.getElementsByClassName('radioButtons')[0];
     var themeMenuOpen = document.getElementsByClassName('gwt-RadioButton classic')[0];
@@ -95,88 +187,5 @@ function status() {
             }
         }
     }
-
-    // From github.com/PoemOnTyperacer/tampermonkey/blob/master/pacemaker.user.js lines 321-339 tyyyy :)
-    let gameStatusLabels = document.getElementsByClassName('gameStatusLabel');
-    let gameStatus = ((gameStatusLabels || [])[0] || {}).innerHTML || '';
-    if (!racing && (gameStatusLabels.length > 0 && (gameStatus == 'Go!' || gameStatus.startsWith('The race is on') || gameStatus == 'The race is about to start!'))) {
-        let practiceTitleEl = document.getElementsByClassName('roomSection')[0];
-        if(practiceTitleEl && practiceTitleEl.innerText.startsWith('Practice')) {
-            raceStart();
-        } else {
-            var countdown = document.getElementsByClassName('lightLabel')[0].parentNode.nextSibling.textContent;
-            if (countdown == ":02") {
-                raceStart();
-            }
-        }
-    }
-
-    if(racing && ((gameStatusLabels.length==0) || (document.getElementsByClassName('rank')[0].innerText=='Done!'||document.getElementsByClassName('rank')[0].innerText.includes('Place')))){
-        raceEnd();
-    }
-
-}
-
-function addNext(textDiv, textSpans) {
-    var hiddenSpan = textSpans[textSpans.length-1];
-    var peekedWords = hiddenSpan.textContent.split(" ", peek+1).join(" ")+" ";
-    var shownWords = document.createTextNode(peekedWords);
-    var currPos = textSpans[textSpans.length-2];
-    var typo = document.getElementsByClassName('txtInput txtInput-error')[0];
-
-    if (currPos.textContent != peekedWords) {
-        var shownSpan = document.createElement("span");
-        if (currPos.textContent == " ") {
-            console.log("otnheun");
-            // At end of word
-            shownWords = document.createTextNode(hiddenSpan.textContent.split(" ", peek).join(" ")+" ");
-            shownSpan.appendChild(shownWords);
-            currPos.parentNode.insertBefore(shownSpan, currPos.nextSibling);
-            shownSpan.className = "nextWords";
-        }
-        if ((currPos.textContent.match(/ /g)||[]).length != peek) { // Avoids adding another shownSpan if at end of word
-            console.log("orhusonathnst");
-            shownSpan.appendChild(shownWords);
-            currPos.parentNode.insertBefore(shownSpan, currPos.nextSibling);
-            shownSpan.className = "nextWords";
-        }
-    }
-}
-
-function peekNext() {
-    var textDiv = document.querySelector('.inputPanel tbody tr td table tbody tr td div');
-    var textSpans = document.querySelectorAll('.inputPanel tbody tr td table tbody tr td div div span');
-
-    // Maintaining initial height
-    textDiv.setAttribute("style", "height: "+initHeight+"px");
-    textDiv.style.position = "relative";
-
-    var hiddenSpan = textSpans[textSpans.length-1];
-    var hiddenSpanNum = (hiddenSpan.textContent.match(/ /g)||[]).length;
-    if (peek >= hiddenSpanNum) {
-        // Read ahead covers rest of text -> quit
-        return;
-    } else {
-        // Disabled during race?
-        if (!enabled) {
-            document.getElementsByClassName("nextWords")[0].remove();
-            raceEnd();
-            return;
-        }
-        // Hide upcoming words
-        textDiv.style.visibility = "hidden";
-        for (var i = 0; i < (textSpans.length-1); i++) {
-            textSpans[i].style.visibility = "visible";
-        }
-    }
-
-    // Don't hide smooth caret in new theme
-    var smoothCaret = document.getElementById("smoothCaret");
-    if (newTheme && smoothCaret) {
-        document.getElementById("smoothCaret").style.visibility = "visible";
-    }
-    addNext(textDiv, textSpans);
-}
-
-loadSettings();
-setInterval(status, 100);
+});
+observer.observe(document, {attributes: false, childList: true, characterData: false, subtree: true});
